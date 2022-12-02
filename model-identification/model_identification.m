d829d3d12053b6210be28e47e93946792800a03c
Theo Chemel <theo@chemel.net>
2022-12-02 17:49:53 -0500

    Implement model

--- model-identification/model_identification.m
+++ model-identification/model_identification.m
@@ -1,7 +1,7 @@
 function [] = model_identification()
 raw_nav = readmatrix('data/bags-12-01-22/navigation_state.csv');
 nav_t = raw_nav(:, 1);
-nav = raw_nav(:, [14, 15, 16, 8, 9, 10, 17, 18, 19]);
+nav = raw_nav(:, [14, 15, 16, 8, 9, 10]);
 
 raw_wrench = readmatrix('data/bags-12-01-22/wrench.csv');
 wrench_t = raw_wrench(:, 1);
@@ -15,7 +15,7 @@ ang_vel = cat(1, diff(raw_nav(:, [14, 15, 16])) / 0.05, [0, 0, 0]);
 ang_accel = cat(1, diff(ang_vel) / 0.05, [0, 0, 0]);
 ang_accel = smoothdata(ang_accel, 'gaussian', 10);
 y = horzcat(lin_accel, ang_accel);
-u = horzcat(nav, wrench);
+u = horzcat(horzcat(nav, ang_vel), wrench);
 
 y(isnan(y)) = 0;
 u(isnan(u)) = 0;
@@ -34,17 +34,32 @@ Order = [6, 15, 0];
 linear_damping = [
     10; 10; 10; 10; 10; 10
 ];
-Parameters = {linear_damping};
+inertias = [
+  1; 1; 1;
+];
+mass = 30;
+volume = 30;
+center_of_gravity = [
+  0; 0; 0  
+];
+center_of_buoyancy = [
+    0; 0; 0
+];
+Parameters = {linear_damping, inertias, mass, volume, center_of_gravity, center_of_buoyancy};
 InitialStates = [];
 Ts = 0;
 
 nlgr = idnlgrey(ModelFile, Order, Parameters, InitialStates, Ts);
+nlgr.Parameters(3).Fixed = true;
 
 z = iddata(y, u, 0.05, 'Name', 'Sub');
 compare(z, nlgr);
 
-opt = nlgreyestOptions(Display='on');
+opt = nlgreyestOptions('Display', 'full');
+opt.SearchMethod = 'lsqnonlin';
+opt.SearchOptions.Advanced.UseParallel = true;
 nlgr_est = nlgreyest(z, nlgr, opt);
+present(nlgr_est);
 figure();
 compare(z, nlgr_est);
 end
--- model-identification/model_linear_damping_m.m
+++ model-identification/model_linear_damping_m.m
@@ -1,16 +1,118 @@
-function [dx, y] = model_linear_damping(t, x, u, L, varargin)
+function [dx, y] = model_linear_damping(t, x, u, L, I, m, v, CG, CB, varargin)
     dx = []; % static model
-
-    mass = 30;
 
     rpy = u(1, 1:3);
     lin_v = u(1, 4:6);
     ang_v = u(1, 7:9);
     tau = u(1, 10:15);
 
-    tau_damping = [
-        L(1:3).' .* lin_v, L(4:6).' .* ang_v
-    ];
+    I_t = buildInertiaTensor(I(1), I(2), I(3), 0, 0, 0);
+    M_rb = buildMassMatrix(m, CG, I_t);
+    C_rb = buildCoriolisMatrix(m, CG, I_t, lin_v.', ang_v.');
+    D_lin = diag([L]);
+    G = buildGravityMatrix(m, v, CG, CB, rpy.');
 
-    y = (tau - tau_damping) / mass; % accelerations
+    y = (M_rb \ (tau.' - G - C_rb * [lin_v.'; ang_v.'] - D_lin * [lin_v.'; ang_v.'])).';
+end
+
+function [J, J1, J2] = buildEulerMatrix(n2)
+    % Note: Due to the use of euler angle representation, J becomes
+    % undefined when theta = +/- 90 degrees.
+    % TODO: switch to a quaternion representation!
+    phi = n2(1);
+    theta = n2(2);
+    psi = n2(3);
+    
+    cphi = cos(phi);
+    sphi = sin(phi);
+    cth = cos(theta);
+    sth = sin(theta);
+    tth = tan(theta);
+    cpsi = cos(psi);
+    spsi = sin(psi);
+    
+    % Prevent singularity condition
+    %eps = 0.001;
+    %cth = max(abs(cth), eps)*sign(cth);
+    
+    % Eq 4.17 (Chin 2013, p 136)
+    J1 = [cpsi*cth, -spsi*cphi + cpsi*sth*sphi, spsi*sphi + cpsi*cphi*sth;
+        spsi*cth, cpsi*cphi + sphi*sth*spsi, -cpsi*sphi + sth*spsi*cth;
+        -sth, cth*sphi, cth*cphi];
+    % J1 = eul2rotm([phi,theta,psi], 'yxz')';
+    
+    % NOTE: There is an error in Chin 2013. J2(2,2) = cphi, not sphi!
+    J2 = [1, sphi*tth, cphi*tth;
+        0, cphi, -sphi;
+        0, sphi/cth, cphi/cth];
+    
+    J = [J1, zeros(3); zeros(3), J2];
+end
+
+function I = buildInertiaTensor(Ixx, Iyy, Izz, Ixy, Ixz, Iyz)
+    % Eq 4.3b: (Chin 2013, p 132)
+    I = [Ixx, -Ixy, -Ixz;
+        -Ixy,  Iyy, -Iyz;
+        -Ixz, -Iyz,  Izz];
+end
+
+function M_rb = buildMassMatrix(m, r_G, I)
+    % Eq 4.6: (Chin 2013, p 132)
+    M_rb = [m * eye(3), -m*skew(r_G);
+            m * skew(r_G), I];
+end
+
+function C_rb = buildCoriolisMatrix(m, r_G, I, v1, v2)
+    % Eq 4.10: (Chin 2013, p 134)
+    C_rb = [zeros(3),   -m*skew(v1) - m*skew(v2)*skew(r_G);
+            -m*skew(v1) + skew(r_G)*skew(v2),   -skew(I*v2)];
+end
+
+function [M_added, C_added] = buildAddedMassCoriolisMatrices(v, Ma_x, Ma_y, Ma_z, Ma_yaw, Ma_pitch, Ma_roll)
+    % eq 4.32: (Chin 2013, p 143)
+    M_added = -diag([Ma_x, Ma_y, Ma_z, Ma_yaw, Ma_pitch, Ma_roll]);
+    
+    % eq 4.33: (Chin 2013, p 143)
+    % TODO: verify correctness!
+    C_added = ...
+        [0, 0, 0, 0, -Ma_z * v(3), Ma_y * v(2);
+         0, 0, 0, Ma_z * v(3), 0, -Ma_x * v(1);
+         0, 0, 0, -Ma_y * v(2), Ma_x * v(1), 0;
+         0, -Ma_z * v(3), Ma_y * v(2), 0, -Ma_roll * v(6), Ma_pitch * v(5);
+         Ma_z * v(3), 0, -Ma_x * v(1), Ma_roll * v(6), 0, -Ma_yaw * v(4);
+         -Ma_y * v(2), Ma_x * v(1), 0, -Ma_pitch * v(5), Ma_yaw * v(4), 0];
+end
+
+function G = buildGravityMatrix(m, b, r_G, r_B, n2)
+    g = 9.81;
+    W = m*g;
+    B = b*g;
+    
+    phi = n2(1);
+    theta = n2(2);
+    cph = cos(phi);
+    sph = sin(phi);
+    cth = cos(theta);
+    sth = sin(theta);
+    
+    x_G = r_G(1);
+    y_G = r_G(2);
+    z_G = r_G(3);
+    x_B = r_B(1);
+    y_B = r_B(2);
+    z_B = r_B(3);
+    
+    % Eq 4.54 (Chin 2013, p 180)
+    G = [(W-B) * sth;
+         -(W-B) * cth*sph;
+         -(W-B) * cth*cph;
+         -(y_G*W - y_B*B)*cth*cph + (z_G*W - z_B*B)*cth*sph;
+         (z_G*W - z_B*B)*sth + (x_G*W - x_B*B)*cth*cph;
+         -(x_G*W - x_B*B)*cth*sph - (y_G *W - y_B*B)*sth;];
+end
+
+function S = skew(x)
+    % Chin 2013, p 133.
+    % Defined as S(x)*y = cross(x,y)
+    S = [0 -x(3) x(2) ; x(3) 0 -x(1) ; -x(2) x(1) 0 ];
 end
